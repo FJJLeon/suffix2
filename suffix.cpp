0ecf6fbd56d383cb1309d692a83bc6e9cfe6f0eb
FJJLeon <695048014@qq.com>
2017-05-08 23:53:30 +0800

    last

--- suffix.cpp
+++ suffix.cpp
@@ -3,10 +3,11 @@
 #include<fstream>
 #include <vector>
 #include <map>
+#include <algorithm>
 
 using namespace std;
 
-void read(vector<string>& word_v) {
+void read(vector<string>& word_v) {//读入文件内容到 word_v 的vector
 	ifstream dict;
 	dict.open("dictionary.txt");
 
@@ -18,8 +19,8 @@ void read(vector<string>& word_v) {
 	dict.close();
 }
 
-bool isexist(vector<string> v, string s) {
-	if (v.size == 0)
+bool isexist(vector<string> v, string s) {//判定一个vector<string>中是否存在某个string
+	if (v.size() == 0)
 		return false;
 
 	for (int i = 0; i < v.size(); i++) {
@@ -29,58 +30,80 @@ bool isexist(vector<string> v, string s) {
 	return false;
 }
 
-struct suffix
+struct suffix   //每种不同的后缀的结构
 {
-	string suffix_str;
-	int suffix_num;
+	string str;
+	int num;
+	vector<string> member;
 };
 
-void group(int n, vector<suffix> struct_v, vector<string>& suffix_v, vector<string>& word_v) {
+void group(int n, vector<suffix>& struct_v, vector<string>& str_v, vector<string>& word_v) {
 
 	for (int i = 0; i < word_v.size(); i++) {
 		if (word_v[i].length() < n)
 			continue;
 		
-		string suffix_str = word_v[i].substr(word_v[i].length() - n, word_v[i].length());
+		string str = word_v[i].substr(word_v[i].length() - n, word_v[i].length());//获取 n -letter suffix
 
-		if (!isexist(suffix_v, suffix_str))//不存在
+		if (!isexist(str_v, str))//不存在
 		{
-			suffix_v.push_back(suffix_str);
-			suffix tmp = { suffix_str ,1 };
-			struct_v.push_back(tmp);
+			str_v.push_back(str);
+			//suffix tmp = { str ,1, vector<string>{word_v[i]} };
+			struct_v.push_back(suffix{ str ,1, vector<string>{word_v[i]} });  //可能有错。。。。。。。
 		}
-		else
-		{
-			for (int i = 0; i < struct_v.size(); i++) {
-				if (struct_v[i].suffix_str == suffix_str)
-					struct_v[i].suffix_num += 1;
+		else{
+			for (int j = 0; j < struct_v.size(); j++) {
+				if (struct_v[j].str == str){
+					struct_v[j].num += 1;
+					struct_v[j].member.push_back(word_v[i]);
+				}				
 			}
 		}
 	}
 }
 
-
+bool myless(suffix&s1, suffix&s2) {
+	return s1.num < s2.num;
+}
 
 int main()
 {
 	vector<string> word_v;
-
 	read(word_v);
 
-	cout << "Please input the number of letters of suffix" << endl;
-	int suffix_n;
-	cin >> suffix_n;
+	cout << "Please input the number of letters of suffix" << endl; //读入 后缀的长度
+	int n;
+	cin >> n;
 
-	vector<string> suffix_v;
-	vector<suffix> struct_v;
+	vector<string> str_v; //直接保存 所有不同的 n -letter suffix
+	vector<suffix> struct_v; //保存 所有 n -letter suffix的结构
 
-	group(suffix_n, struct_v, suffix_v, word_v);
+	group(n, struct_v, str_v, word_v);
 	
-	for (int i = 0; i < suffix_v.size(); i++) {
+	sort(struct_v.begin(), struct_v.end(), myless);
 
+	for (int i = struct_v.size()-1; i > struct_v.size() - 11; i--) {
+		cout << "top" << struct_v.size()-i << "  " << struct_v[i].str << " number is" << struct_v[i].num<<endl;
 	}
 
-
+	cout << "Now you can query a n-letter suffix by inputting the  suffix"<<endl;
+	while (true) {
+		string tmp;
+		cin >> tmp;
+		if (tmp == "quit") break;
+		int pos;
+		for (int i = 0; i < struct_v.size(); i++) {
+			if (struct_v[i].str == tmp) {
+				pos = i;
+				break;
+			}				
+		}
+		sort(struct_v[pos].member.begin(), struct_v[pos].member.end());
+		for (int i = struct_v[pos].member.size()-1; i >=0; i--) {
+			cout << struct_v[pos].member[i] << " ";
+		}
+		cout << endl << "Now you can query next(input quit to quit)";
+	}
 
 	system("PAUSE");
 }
